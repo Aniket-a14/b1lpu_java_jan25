139e11e91127d99a08a0cc1f5d3c1981d72cb4fd
Aniket Saha <aniketsahaworkspace@gmail.com>
2025-01-28 16:45:48 +0530

    add main method to User class for testing user details

--- Day5/User.java
+++ Day5/User.java
@@ -33,4 +33,22 @@ public class User {
     public  void setBalance(double balance){
         this.balance = balance;
     }
+
+    public static void main(String[] args) {
+        // Default constructor
+        User user1 = new User();
+        
+        
+        User user2 = new User("John Doe", "123456789");
+
+        // Set details
+        user2.setPhnNo("9876543210");
+        user2.setBalance(1000.50);
+
+        // Print details
+        System.out.println("Name: " + user2.getName());
+        System.out.println("Phone Number: " + user2.getPhnNo());
+        System.out.println("Account Number: " + user2.getAccNo());
+        System.out.println("Balance: " + user2.getBalance());
+    }
 }
